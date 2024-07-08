41216f8e82bfc46641a130be847d01115167b078
julia-retuerta <164010086+julia-retuerta@users.noreply.github.com>
2024-07-08 11:01:16 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,20 @@
+import copy from 'rollup-plugin-copy';
 import { defineConfig } from 'vite';
 import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
 import { ViteMinifyPlugin } from 'vite-plugin-minify';
-
 export default defineConfig({
-  plugins: [ViteMinifyPlugin({}), ViteImageOptimizer({})],
-  base: '',
+  plugins: [
+    ViteMinifyPlugin({}),
+    ViteImageOptimizer({
+      include: ['**/*.png', '**/*.jpg', '**/*.jpeg']
+    }),
+    copy({
+      targets: [{ src: 'src/assets/images/*.svg', dest: 'docs/assets' }],
+      hook: 'writeBundle' // Aseguramos que los archivos se copien después de que el bundle se haya generado
+    })
+  ],
   root: 'src',
   build: {
-    outDir: '../docs',
-  },
+    outDir: '../docs'
+  }
 });
